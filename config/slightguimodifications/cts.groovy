c3d319a08e7bed1c7c8cc06db192521db813d8d6
MSandro <sa08ma@gmail.com>
2022-04-01 07:05:21 +0200

    version bump

--- config/slightguimodifications/cts.groovy
+++ config/slightguimodifications/cts.groovy
@@ -29,7 +29,7 @@ mainMenu {
             }
         }
 
-        text = literal("Sky FABRICation 2 1.1.0-Pre1")
+        text = literal("Sky FABRICation 2 1.1.0-Pre2")
         shadow = true
     }
 
